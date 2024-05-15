696c21d20487de3d1e1e34c8f33771b1953addf9
seongRyoung <tjdfyd6619@gmail.com>
2024-05-16 00:39:21 +0900

    spread operator React

--- react-project/src/components/TabButton.jsx
+++ react-project/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? 'actvie' : undefined} onClick={onSelect}>
+      <button className={isSelected ? 'actvie' : undefined} {...props}>
         {children}
       </button>
     </li>
