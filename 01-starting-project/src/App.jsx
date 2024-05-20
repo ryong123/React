b7a6e08b0832d559d20471222d0663267ab0bb7a
seongRyoung <tjdfyd6619@gmail.com>
2024-05-21 00:25:51 +0900

    React

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,33 +1,11 @@
 import Header from './components/Header';
+import UserInput from './components/UserInput';
 
 function App() {
   return (
     <>
       <Header></Header>
-      <div id="user-input">
-        <div className="input-group">
-          <label>
-            INTIAL INVESTMENT
-            <input type="number" />
-          </label>
-          <label>
-            ANNUAL INVESTMENT
-            <input type="number" />
-          </label>
-        </div>
-        <div className="input-group">
-          <label>
-            INTIAL INVESTMENT
-            <input type="number" />
-          </label>
-          <label>
-            ANNUAL INVESTMENT
-            <input type="number" />
-          </label>
-        </div>
-
-        <div id="result"></div>
-      </div>
+      <UserInput></UserInput>
     </>
   );
 }
--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import mainImg from '../assets/investment-calculator-logo.png';
+import logo from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
   return (
     <header id="header">
-      <img src={mainImg} alt="" />
-      <h1>React Investment Calculator</h1>
+      <img src={logo} alt="Logo showing a money" />
+      <h1>Investment Calculator</h1>
     </header>
   );
 }
