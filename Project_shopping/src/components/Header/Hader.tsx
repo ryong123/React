e0b3ad770a0a30410fd66147c3efc9e3dddeaa4b
seongRyoung <tjdfyd6619@gmail.com>
2024-05-24 02:19:33 +0900

    Header Component

--- Project_shopping/src/components/Header/Hader.tsx
+++ Project_shopping/src/components/Header/Hader.tsx
@@ -2,8 +2,6 @@ import { Link } from 'react-router-dom';
 import Nav from './Nav';
 import { _ContainerDiv, _HeaderWrapper, _LogoDiv } from './styles';
 
-// 헤더 메인 Div
-
 export default function Header() {
   return (
     <_ContainerDiv>
